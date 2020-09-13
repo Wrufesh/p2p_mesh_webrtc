1d23e153ec7578c6538cbdb86f6ed15dbbefa40c
Rupesh Singh <wrufesh@gmail.com>
2020-09-13 19:53:01 +0545

    mf

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -21,7 +21,7 @@
       <h3>Instruction</h3>
       <p>Think of any <b>ROOM ID</b>.</p>
       <p>
-        Enter you <b>DISPLAY NAME</b> and the <b>ROOM ID</b> you thought of.
+        Enter your <b>DISPLAY NAME</b> and the <b>ROOM ID</b> you thought of.
       </p>
       <p>Ask your friends to join with the same <b>ROOM ID</b>.</p>
       <p>You will be connected with your friends in full peer-to-peer room.</p>

aaf26ce8424c76d71bbf0b55d0c246f8c4652730
Rupesh Singh <wrufesh@gmail.com>
2020-09-13 19:47:15 +0545

    Instructions added

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,7 @@
   <form @submit="login">
     <div>
       <div>
-        <label><small>USER ID</small></label>
+        <label><small>DISPLAY NAME</small></label>
       </div>
       <input v-model="userId" required />
     </div>
@@ -16,6 +16,15 @@
     <br />
     <div>
       <button type="submit">PROCEED</button>
+    </div>
+    <div>
+      <h3>Instruction</h3>
+      <p>Think of any <b>ROOM ID</b>.</p>
+      <p>
+        Enter you <b>DISPLAY NAME</b> and the <b>ROOM ID</b> you thought of.
+      </p>
+      <p>Ask your friends to join with the same <b>ROOM ID</b>.</p>
+      <p>You will be connected with your friends in full peer-to-peer room.</p>
     </div>
   </form>
 </template>
@@ -36,11 +45,14 @@ export default defineComponent({
     ...mapActions(["setUserAndRoom"]),
     login() {
       if (this.userId && this.roomId) {
-        this.setUserAndRoom({ userId: this.userId, roomId: this.roomId });
+        this.setUserAndRoom({
+          userId: this.userId,
+          roomId: this.roomId.replace(/\s/g, "")
+        });
         this.$router.push({
           name: "Room",
           params: {
-            room: this.roomId
+            room: this.roomId.replace(/\s/g, "")
           }
         });
       }
